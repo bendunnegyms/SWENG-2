35866763f87e0874dd8e7a0a0168ecc4a0403d62
bendunnegyms <syrtsovv@tcd.ie>
2020-11-11 14:37:40 +0000

    starting tests

--- DAGpy/DAG.py
+++ DAGpy/DAG.py
@@ -1,24 +1,53 @@
 class DAG:
     DAG = {}
+    source = None
     reversed_dag = {}
     def __init__(self):
         self.DAG.clear()
     
     def add_edge(self, k1,k2):
+        if len(self.DAG) == 0:
+            self.source = k1
 
+        if k1 == k2:
+            print("Edge not valid - loops on itself.")
+            return False
         if k1 not in self.DAG:
+            if self.__disjoint_check(k1, k2):
+                print("Disjointed.")
+                return False
+            
             self.DAG[k1] = [k2]
+            self.DAG[k2] = []
             if not self.__validate:
                 del self.DAG[k1]
+                del self.DAG[k2]
+                print("Edge not valid. Loop created")
                 return False
             else:
                 return True
         else:
+            if self.__disjoint_check(k1, k2):
+                print("Disjointed.")
+                return False
+            
             self.DAG[k1].append(k2)
+            self.DAG[k2] = []
             if not self.__validate:
                 self.DAG[k1].remove(k2)
-                print("Edge not valid. Loop created or disjointed.")
-    
+                del self.DAG[k2]
+                print("Edge not valid. Loop created")
+                return False
+            else:
+                return True
+        
+    def __disjoint_check(self, k1, k2):
+        # tests for if k1 or k2 arent in the DAG already, if DAG is not empty
+        if k1 not in self.DAG and k2 not in self.DAG and len(self.DAG) > 0:
+            return True
+        else:
+            return False
+
 
     def __validate(self, key):
         vol_dag = self.DAG.copy()
@@ -101,3 +130,12 @@ class DAG:
             if len(dag[key])==0:
                 return key
 
+
+class TestDAGLCA:
+    dag_main = DAG()
+
+    def test_add_edge(self):
+        assert self.dag_main.add_edge(0,0) == False
+        assert self.dag_main.add_edge(0,1) == True
+        assert self.dag_main.add_edge(1,0) == False
+        
